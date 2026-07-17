68c13a4a98e66130dc7a8802a8c6f2ddbafef373
KhunAlek <abystrov66@gmail.com>
2026-07-17 15:30:44 +0700

    Load beginner-safe M7 lesson

--- assets/js/missions-data.js
+++ assets/js/missions-data.js
@@ -24,6 +24,7 @@ if(document.body.dataset.page==="mission"){
   document.write('<script src="assets/js/mission-lessons-m04.js"><\/script>');
   document.write('<script src="assets/js/mission-lessons-m05.js"><\/script>');
   document.write('<script src="assets/js/mission-lessons-m06-m15.js"><\/script>');
+  document.write('<script src="assets/js/mission-lesson-m07-safe-commands.js"><\/script>');
   document.write('<script src="assets/js/m03-evidence-simplification.js"><\/script>');
   document.write('<script src="assets/js/mission-runtime.js"><\/script>');
 }
